1baf0126127e24467647a262ce8983df3eb1e6a8
Muji Rahman <mujirahman888@gmail.com>
2020-12-06 13:06:37 +0700

    melakukan konfigurasi awal instalasi typescript tailwind react router dan axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/output.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// interface custum {
+//   // id: string;
+
+// }
+
+function App () {
+  return <h1 className="text-2xl bg-black text-white"> saya lagi nganggur</h1>;
 }
 
 export default App;
